572e2d45856b1259ac61ea88374290201dfa20c4
Nguyen Gia Huy <nguyengiahuy27201@gmail.com>
2022-09-25 14:07:42 +0700

    Add some extra features

---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square({ value, onClick, winSquare }) {
+    return (
+        <button className={winSquare ? 'square win-square' : 'square'} onClick={onClick} style={{}}>
+            {value}
+        </button>
+    );
+}
+
+export default Square;
